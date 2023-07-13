64aedb4817e92ad9b61d3e9ee453fe01f7b76bf7
Fabio Fiuza <fabiio.fiuza@gmail.com>
2023-07-13 11:25:57 -0300

    add testes de verificação

--- djangoProject/core/verification.py
+++ djangoProject/core/verification.py
@@ -14,17 +14,31 @@ import pandas as pd
 #print(dicionario)
 
 r1 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "5", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Segunda"}
-r2 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "3", "Bloco": "2A", "Horario": "10:00-12:00", "Semana": "Terca"}
-r3 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "3", "Bloco": "2A", "Horario": "13:30-15:30", "Semana": "Terca"}
-r4 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "5", "Bloco": "1A", "Horario": "15:30-17:30", "Semana": "Terca"}
-r5 = {"Professor": "Reuber", "Disciplina": "PAA", "Sala": "3", "Bloco": "2A", "Horario": "13:30-15:30", "Semana": "Terca"}
+r2 = {"Professor": "Tatiane", "Disciplina": "ED", "Sala": "3", "Bloco": "2A", "Horario": "10:00-12:00", "Semana": "Terca"}
+r3 = {"Professor": "Rafael Ivo", "Disciplina": "Computação Gráfica", "Sala": "3", "Bloco": "2A", "Horario": "13:30-15:30", "Semana": "Terca"}
+r4 = {"Professor": "Eurinardo", "Disciplina": "LFA", "Sala": "1", "Bloco": "1A", "Horario": "08:10-10:00", "Semana": "Terca"}
+r5 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "5", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Segunda"}
+r6 = {"Professor": "Reuber", "Disciplina": "SO", "Sala": "5", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Segunda"}
+r7 = {"Professor": "Eurinardo", "Disciplina": "EDA", "Sala": "7", "Bloco": "2A", "Horario": "8:00-10:00", "Semana": "Segunda"}
+r8 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "5", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Segunda"}
 
+r9 = {"Professor": "Felipe", "Disciplina": "POO", "Sala": "4", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Quarta"}
+r10 = {"Professor": "Anderson", "Disciplina": "Algebra Linear", "Sala": "4", "Bloco": "1A", "Horario": "8:00-10:00", "Semana": "Quarta"}
+r11 = {"Professor": "Eurinardo", "Disciplina": "EDA", "Sala": "4", "Bloco": "1A", "Horario": "13:30-15:30", "Semana": "Quarta"}
+r12 = {"Professor": "Eurinardo", "Disciplina": "PAA", "Sala": "4", "Bloco": "2A", "Horario": "8:00-10:00", "Semana": "Segunda"}
+r13 = {"Professor": "Alexandre", "Disciplina": "Lógica", "Sala": "4", "Bloco": "1A", "Horario": "15:30-17:30", "Semana": "Quarta"}
+r14 = {"Professor": "Reuber", "Disciplina": "SD", "Sala": "4", "Bloco": "1A", "Horario": "10:00-12:00", "Semana": "Quarta"}
 
-banco = [r1, r2, r3, r4]
+teste = [r1, r2, r3, r4]
+teste2 = [r10, r11, r12]
+teste3 = [r10, r11, r12, r13, r14]
 
 #retorna_horarios_disponiveis(dicionario5)
 
-df = pd.DataFrame(banco)
+df = pd.DataFrame(teste)
+df2 = pd.DataFrame(teste2)
+df3 = pd.DataFrame(teste3)
+
 
 def verificar_registro_existente(df, novo_registro):
     registro_existente = df[df.eq(novo_registro).all(axis=1)].shape[0] > 0
@@ -35,7 +49,7 @@ def verificar_registro_existente(df, novo_registro):
         return 0 # False
 
 def verificar_conflito_sala(df, registro):
-    if len(df.loc[(df['Bloco']) == registro['Bloco'] & (df['Sala'] == registro['Sala']) & (df['Semana'] == registro['Semana']) & (df['Horario'] == registro['Horario'])]) == 0:
+    if len(df.loc[(df['Bloco'] == registro['Bloco']) & (df['Sala'] == registro['Sala']) & (df['Semana'] == registro['Semana']) & (df['Horario'] == registro['Horario'])]) == 0:
         return 0
     else:
        return 1
@@ -69,6 +83,7 @@ def horarios_disponiveis(df, novo_registro):
     horarios_disponiveis = []
     
     horarios = ['8:00-10:00', '10:00-12:00', '13:30-15:30', '15:30-17:30']
+    
     for horario in horarios:
         if horario not in horarios_preenchidos:
             horarios_disponiveis.append(horario)
@@ -77,14 +92,24 @@ def horarios_disponiveis(df, novo_registro):
         print("Horário disponível")
     else:
         print("Esse horário não está disponível")
-        print(f"Horários disponíveis: {horarios_disponiveis}")
+        if len(horarios_disponiveis) == 0:
+            print("Não existem horários disponíveis para esse dia")
+        else:
+            print(f"Horários disponíveis: {horarios_disponiveis}")
         
-    #x = filtro[horarios] in horarios
-    #print(filtro['Horario'])
-    #horarios_disponiveis = [horario for horario in horarios if horario not in list(filtro['Horario'])]
-    #print(horarios_disponiveis)
-    
-horarios_disponiveis(df, r5)
-#alocar_novo_registro(df, r5)
+ 
+#teste 1 Registro já alocado
+#alocar_novo_registro(df, r5) 
 
-    
+#teste 2 Registro já está sendo utilizada
+#alocar_novo_registro(df, r6) 
+
+#teste 3 Professor já está ocupado nesse horário
+#alocar_novo_registro(df, r7)
+
+#teste 4 Mostrando horários disponíveis
+#horarios_disponiveis(df2, r9)
+
+#teste 5 nenhum horário disponível naquele dia
+#horarios_disponiveis(df3, r9)
+
