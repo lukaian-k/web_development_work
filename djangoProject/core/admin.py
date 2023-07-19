72514589ee08a4ed531e9c465c2b2bec431b7db3
AlanisOliveira <alanisoliveira39@gmail.com>
2023-07-19 16:34:03 -0300

    inserindo adm

--- djangoProject/core/admin.py
+++ djangoProject/core/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from .models import Professor, Sala, Alocacao
+from .models import Professor, Sala, Alocacao, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('id_user', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    list_display = ['id_user', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['id_user']
+    ordering = ['id_user']
+    filter_horizontal = ()
+    readonly_fields = ['id_user']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('id_user', 'password1', 'password2'),
+        }),
+    )
 
 
 @admin.register(Professor)
